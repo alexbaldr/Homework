f40d065478e4fd5480d501891e40de99eef6388a
Alex Khagush <baldr11@mail.ru>
2020-06-09 20:51:24 +0300

    commit

--- VK/friendsVK.py
+++ VK/friendsVK.py
@@ -1,19 +1,21 @@
 import requests
 from pprint import pprint
 import time
+import json
 
 TOKEN = "958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008"
 
 USER_id = 171691064
 
-b = set()
 
 class USER:
+     
     def __init__(self, token):
         self.token = token
 
     def get_grups(self):
 
+        a = set()
         params = {
         'access_token': TOKEN,
         'v': 5.107,
@@ -26,13 +28,10 @@ class USER:
 
         data = response.json()["response"]
         get_items = data.get("items")
-        global a
-        a = set(get_items)     
+        for i in get_items:
+            a.add(i)    
 
-        return a
-
-    def get_friends(self):
-        
+        b = set()
         params = {
         'access_token': TOKEN,
         'v': 5.107,
@@ -44,12 +43,8 @@ class USER:
         response = requests.get("https://api.vk.com/method/friends.get", params)
         
         data = response.json()["response"]
-        #print(data)
         get_items = data.get("items")
-        #print(get_items)
-        # УБИРАЕМ ВСЕХ УДАЛЕННЫХ И ЗАБЛОКИРОВАННЫХ ПОЛЬЗОВАТЕЛЕЙ (СКОРЕЕ ВСЕГО ПЕРЕИЗБЫТОЧНАЯ ОПЦИЯ)
         for i in get_items:
-            #for  v in i.values():
             if i["first_name"] != 'DELETED': 
                 get_id = i["id"]
                 list_of_friends.append(get_id)
@@ -60,7 +55,7 @@ class USER:
                 'v': 5.107,
                 "user_id": id_f,
                 "extended": 0,
-                "count": 1000
+                "count": 2
                     }
 
             repeat = True
@@ -85,22 +80,59 @@ class USER:
                     get_items_of_groups = list_of_groups.get("items")
                     for i in get_items_of_groups:
                         b.add(i)
-
-    
-    def exeptions_of_grups(self):
         x = a & b
-        print(x)
-        
-
-
-
+        return x
     def get_json(self):
+        #ПОЛУЧАЕМ ДАННЫЕ ГРУПП
+        x = {35486626, 134709480, 125927592, 4100014, 101522128, 8564, 27683540}
+        for i in x:
+            params = {
+            'access_token': TOKEN,
+            'v': 5.107,
+            "group_ids": i
+            }
 
-        return
+            response = requests.get(
+                'https://api.vk.com/method/groups.getById', params=params)
+            
+            res = response.json()["response"]
+            for i in res:
+                name = i.get("name")
+                group_id = i.get("id")
+                #print(name,group_id)
 
+        # ПОЛУЧАЕМ ID ГРУПП
+        for i in x:
+            params = {
+            'access_token': TOKEN,
+            'v': 5.107,
+            "group_id":i,
+            "count":1
+            }
+            repeat = True
+            response = requests.get(
+                'https://api.vk.com/method/groups.getMembers', params=params)
+            members_count = response.json()
+            if 'error' in members_count :
+                time.sleep(2)   
+            else:
+                repeat = False
+            if "error" not in members_count:
+                members_count_list = members_count["response"]["count"]
+                #print (members_count_list)
+
+        merged = [{"name": name,
+                    "gid": group_id,
+                    "members_count": members_count_list}]
+
+        for i in merged:
+            gotten_groups = []  
+            gotten_groups.append(i)
+            #with open('groups.json', 'w',encoding = 'UTF-8') as f:
+            #    json.dump(merged,f,ensure_ascii = False)            
+
+            return gotten_groups
 
 Evg = USER(TOKEN)
-
-Evg.get_grups()
-Evg.get_friends()
-print(Evg.exeptions_of_grups())
+print(Evg.get_grups())
+#print(Evg.get_json())
